8d88fc56e8835af7a9524ef75468b5f4ba628b17
victorpena13 <victor.pena1392@gmail.com>
2020-11-24 17:49:40 -0600

    ads table is now joined to a users id and a category id

--- tables.sql
+++ tables.sql
@@ -1,22 +1,32 @@
 use listcraig_db;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ads;
 
 CREATE TABLE users (
-    id int unsigned not null auto_increment primary key,
+    id int unsigned not null auto_increment,
     name varchar(20),
     email varchar(75),
-    password varchar(20)
+    password varchar(20),
+    primary key(id)
 );
 
+CREATE TABLE categories (
+    id int unsigned not null,
+    name varchar(20),
+    primary key(id)
+);
 
 CREATE TABLE ads (
     id int unsigned not null auto_increment,
     user_id int unsigned not null,
+    categories_id int unsigned not null,
     title varchar(500),
     description text,
     primary key (id),
-    foreign key (user_id) references users(id)
+    foreign key (user_id) references users(id),
+    foreign key (categories_id) references categories(id)
 );
 
 
